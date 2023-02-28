cc887a6fbc815958aecded7ad935242705501862
Andy Lu <andylu@Andys-MacBook-Pro.local>
2023-02-28 16:32:15 -0500

    some example code from android official

--- app/src/main/java/com/example/fooddeliverycomparison/MainActivity.kt
+++ app/src/main/java/com/example/fooddeliverycomparison/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.fooddeliverycomparison
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.material.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fooddeliverycomparison.ui.theme.FoodDeliveryComparisonTheme
@@ -21,26 +26,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FoodDeliveryComparisonTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("World")
-                }
+                DiceRollerApp()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Surface(color = Color.Magenta) {
-        Text(text = "my name is $name!", modifier = Modifier.padding(24.dp));
-    }
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center))
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    FoodDeliveryComparisonTheme {
-        Greeting("Stephen")
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf( 1) }
+    val imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = result.toString()
+        )
+        Spacer(modifier = Modifier.height(64.dp))
+        Button(onClick = { /*TODO*/
+            result = (1..6).random()
+        }) {
+            Text(stringResource(R.string.roll))
+        }
     }
 }
