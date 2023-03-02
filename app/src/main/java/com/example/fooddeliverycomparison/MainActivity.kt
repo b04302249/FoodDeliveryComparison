702ef7ed152f0a3e16f6182bb196a7010b2e8460
Stephen <b04302249@ntu.edu.tw>
2023-03-02 13:22:22 -0500

    first version of food card

--- app/src/main/java/com/example/fooddeliverycomparison/MainActivity.kt
+++ app/src/main/java/com/example/fooddeliverycomparison/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.material.Button
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.*
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fooddeliverycomparison.ui.theme.FoodDeliveryComparisonTheme
@@ -25,43 +32,90 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FoodDeliveryComparisonTheme {
-                DiceRollerApp()
+            Column() {
+                val burgerPainter = painterResource(id = R.drawable.burger)
+                ItemCard(painter = burgerPainter,"$9.99", "burger")
+
+                val friesPainter = painterResource(id = R.drawable.fries)
+                ItemCard(painter = friesPainter,"$19.99", "fries")
+            }
+
+        }
+    }
+}
+
+@Composable
+fun ItemCard(
+    painter: Painter,
+    foodPrice: String,
+    foodName: String,
+){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+        elevation = 8.dp,
+        border = BorderStroke(2.dp,Color.Black)
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp)
+
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Image(
+                    painter = painter,
+                    contentDescription = foodName,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .clip(shape = RoundedCornerShape(8.dp))
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Column() {
+                        Text(
+                            text = foodName,
+                            style = MaterialTheme.typography.h6
+                        )
+                        Text(
+                            text = foodPrice,
+                            style = MaterialTheme.typography.subtitle1,
+                            modifier = Modifier.padding(top = 8.dp)
+                        )
+                    }
+                    Button(
+                        onClick = { /* ... */ },
+                        // Uses ButtonDefaults.ContentPadding by default
+                        contentPadding = PaddingValues(
+                            start = 20.dp,
+                            top = 12.dp,
+                            end = 20.dp,
+                            bottom = 12.dp
+                        )
+                    ) {
+                        // Inner content including an icon and a text label
+                        Icon(
+                            Icons.Filled.Favorite,
+                            contentDescription = "Favorite",
+                            modifier = Modifier.size(ButtonDefaults.IconSize)
+                        )
+                        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                        Text("Like")
+                    }
+                }
+
             }
         }
     }
+
 }
 
-@Preview
-@Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center))
-}
 
-@Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf( 1) }
-    val imageResource = when(result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
-    Column (
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(imageResource),
-            contentDescription = result.toString()
-        )
-        Spacer(modifier = Modifier.height(64.dp))
-        Button(onClick = { /*TODO*/
-            result = (1..6).random()
-        }) {
-            Text(stringResource(R.string.roll))
-        }
-    }
-}
+
+
